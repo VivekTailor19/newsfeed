bbf3d303edcdc03ef63abbcbaf767762a13592e6
Vivek <tailorvivekgec19@gmail.com>
2023-05-25 15:05:31 +0530

    news app

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 import '../model/newsModel.dart';
 import '../provider/newsprovider.dart';
@@ -17,12 +18,49 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-
     newsT = Provider.of<NewsProvider>(context);
-    newsF = Provider.of<NewsProvider>(context,listen: false);
-
+    newsF = Provider.of<NewsProvider>(context, listen: false);
 
     return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+
+            Container(height: 50.h,width: double.infinity, color: Color(0xff151924),),
+
+            Padding(
+              padding: EdgeInsets.all(15.sp),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Image.asset("assets/images/logosmall.png",fit: BoxFit.fill,height: 25.sp,width: 25.sp,),
+                      SizedBox(width: 2.w,),
+                      Row(mainAxisSize: MainAxisSize.min,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text("News",style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600,color: Color(0xffFFFFFF)),),
+                          Text("Feed",style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600,color: Color(0xffFFFFFF))),
+                        ],
+                      ),
+                      Spacer(),
+                      Icon(Icons.notifications_active_outlined,color: Colors.white,)
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/*
+
+SafeArea(
       child: FutureBuilder(
         future: newsF!.loadNews("in"),
         builder: (context, snapshot) {
@@ -52,10 +90,10 @@ class _HomeScreenState extends State<HomeScreen> {
                  ),
                );
             }
-            
+
           return Scaffold(body: Center(child: CircularProgressIndicator()));
         },
       )
     );
-  }
-}
+
+*/
